ace9b8f8fd32cf860987b6bed4f68104dc73966c
Davidcito12 <92931033+Davidcito12@users.noreply.github.com>
2022-05-09 03:22:21 -0500

    Un poco de todo sobre mis cosas (david)

--- Practica1_i-Lunch/src/gestorAplicacion/usuariosRestaurante/Cliente.java
+++ Practica1_i-Lunch/src/gestorAplicacion/usuariosRestaurante/Cliente.java
@@ -1,5 +1,13 @@
 package gestorAplicacion.usuariosRestaurante;
 
-public class Cliente {
+import java.time.LocalDateTime;
 
+public class Cliente {
+	private int telefono;
+	private String primerNombre;
+	private String segundoNombre;
+	private String primerApellido;
+	private String segundoApellido;
+	private String direccion;
+	private LocalDateTime fechaNacimientol;
 }
