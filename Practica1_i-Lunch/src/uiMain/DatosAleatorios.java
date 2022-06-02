95f92e0a05378e604d0a0b833409835b00655e33
Emmanuel López <lopezrodemmanuel@gmail.com>
2022-06-01 22:04:30 -0500

    Base de datos aleatorios definitiva

--- Practica1_i-Lunch/src/uiMain/DatosAleatorios.java
+++ Practica1_i-Lunch/src/uiMain/DatosAleatorios.java
@@ -1,21 +1,35 @@
 package uiMain;
 
 public class DatosAleatorios {
-	// Nombres aleatorios para empleados
-	public static String[] nombresAleatorios = { "Yasemin Phillips", "Lavinia Banks", "Kodi Paine", "Phebe Beaumont",
-			"Amba Bowman", "Mila-Rose Bartlett", "Alejandro Ventura", "Zavier Burns", "Rivka Cantu",
-			"Anastasia Mckeown", "Vivaan Craft", "Leonie Hardin", "Naima Peck", "Sammy-Jo Hollis", "Gus Jefferson",
-			"Tate Greene", "Jai Holmes", "Ella-Mai Moran", "Clayton Hanna", "Guy Holding", "Cherie Valentine",
-			"Zakariah Copeland", "Jane Mccartney", "Iosif Morrison", "Moshe Fenton", "Kylie Mcguire", "Mikael Caldwell",
-			"Eshaal Zavala", "Amanpreet Galloway", "Marek Rocha", "Maison Alvarado", "Conner Contreras",
-			"Macie Needham", "Huseyin Bassett", "Nuala Arroyo", "Brendan Whitworth", "Armaan Espinosa",
-			"Danielius Bravo", "Maja Atherton", "Bryson Nava", "Mateusz Mackie", "Daphne Lim", "Duane Sargent",
-			"Una Graham", "Sioned Crane", "Rogan Milne", "Teri Porter", "Garrett Vazquez", "Dominika Harvey",
-			"Giselle Croft" };
+	// Nombres aleatorios para empleados y clientes
+	public static String[] nombresAleatorios = { "Phoebe Bright", "Nikolas Beltran", "Jimmy Harrell", "Onur Macias",
+			"Russell Oconnor", "August Roberson", "Madeline Yang", "Dilan Lutz", "Sorcha Sharples", "Chase Parker",
+			"Bob Quinn", "Veronika O'Neill", "Conall Holloway", "Hermione Durham", "Calista Morton", "Yolanda Thatcher",
+			"Suhail O'Brien", "Kayden Reeve", "Carlo Wells", "Rosemarie Cortes", "Kelsie Strickland", "Jaheim Wickens",
+			"Piper Whittaker", "Roy Barker", "Carter Sumner", "Jayda Chaney", "Heath Thomas", "Devan Lopez",
+			"Leopold Simpson", "Domonic Neal", "Verity Shah", "Chaya Ruiz", "Niyah Mercado", "Gerrard Gonzalez",
+			"Izabelle Field", "Ashton Solis", "Aimee Nelson", "Abbey Howells", "Giovanni Dawson", "Kason Kline",
+			"omas Whittington", "Dominika Mccallum", "Roxy Kouma", "Faizaan Mcdougall", "Sahara Sweet", "Gage Humphrey",
+			"Marcie Dodd", "Danyal Howe", "Asiyah Rivers", "Isla-Grace Macgregor", "Sammy Love", "Pippa Simons",
+			"Sarina Best", "Finn Clements", "Adnaan Bradley", "Kenzo Allman", "Ayat Iles", "Caspar Rice",
+			"Arif Aguilar", "Pearce Reyes", "Jorge Cantrell", "Joanna Lim", "Dwayne Mcmanus", "Addie Felix",
+			"Charis Clegg", "Miah O'Moore", "Tayyab Thorpe", "Hannah Watts", "Milli Laing", "Huda Mcmahon",
+			"Keziah Decker", "Theia Glover", "Isobel Henderson", "Marsha Andersen", "Soraya Roman", "Radhika Camacho",
+			"Clara Hopper", "Halimah Derrick", "Evie-Grace Forbes", "Kieren Hickman", "Viola Wilkinson",
+			"Adriana Navarro", "Saba Mckenzie", "Aaminah Poole", "Kalum Plant", "Ursula Mosley", "Nour Maynard",
+			"Frances Everett", "Umayr Wheatley", "Leona Small", "Kylan Zhang", "Aneesah Burgess", "Saara Redmond",
+			"Princess Fletcher", "Margie David", "Hari Langley", "Kayson Stark", "Shana Medrano", "Caoimhe Talley",
+			"Zaid Hawkins" };
 
 	// Nombres aleatorios para productos
-	public static String[] productosAleatorios = { "Pizza", "Hamburguesa", "Bandeja Paisa", "Sushi", "Empanadas",
-			"Pollo frito", "Spaghetti", "Paella", "Tacos" };
+	public static String[] productosAleatorios = { "Pizza sin pinha", "Hamburguesa", "Bandeja Paisa", "Sushi",
+			"Empanadas", "Pollo frito", "Spaghetti", "Paella", "Tacos", "Fondue", "Burrito", "Chop Suey", "Ratatouille",
+			"Ceviche", "Risotto", "Papas fritas", "Lasagna", "Arepas", "Kebab", "Langosta", "Donas", "Curry",
+			"Chicken Teriyaki", "Ramen", "Tamales", "Tortilla", "Shawarma", "Hummus", "Ajiaco", "Fish and Chips",
+			"Rollitos de Primavera", "Buhuelos", "Pozole", "Milanesa", "Croissant", "Helado", "Fideos", "Tofu",
+			"Sashimi", "Costillas BBQ", "Cupcake", "Pastel", "Brownies (Legales)", "Nachos", "Pastas", "Mac and Cheese",
+			"Ensalada", "Quesadilla", "Galletas", "Fajitas", "Macarrones", "Churrasco", "Tiramisu", "Tarta de Manazana",
+			"Churros", "Hot Dog" };
 
 	// Tipos de cargos en la cocina
 	public static String[] cargosEnCocina = { "Chef ejecutivo", "Chef general", "Chef repostero", "Lavaplatos" };
@@ -26,7 +40,9 @@ public class DatosAleatorios {
 
 	// Tipos de vehiculos
 	public static String[] tiposVehiculos = { "Automovil", "Motoicicleta", "Bicicleta", "Cuatrimoto", "Monopatin",
-			"Helicoptero", "Dirigible", "Globo aerostatico", "Tanque de guerra", "Excavadora industrial", "Tractor" };
+			"Helicoptero de la Policia", "Zeppelin", "Globo Aerostatico", "Tanque de Guerra Sovietico",
+			"Excavadora Industrial", "Tractor", "Helicoptero de Guerra Apache AH-64", "Xavineta",
+			"Monoplaza de Formula 1", "OVNI", "Papamovil" };
 
 	// Metodo util para sacar ints randoms
 	public static int randInt(int min, int max) {
