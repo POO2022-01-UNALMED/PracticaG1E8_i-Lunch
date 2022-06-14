dadb5a3d6b3e98d4bab6293c0c2a20acc6d05e88
Davidcito12 <92931033+Davidcito12@users.noreply.github.com>
2022-06-14 02:32:19 -0500

    Cliente terminado

--- Practica2_i-Lunch/src/gestorAplicacion/usuariosRestaurante/cliente.py
+++ Practica2_i-Lunch/src/gestorAplicacion/usuariosRestaurante/cliente.py
@@ -8,16 +8,44 @@ class Cliente(Usuario):
 
     # Constructor
 
-    def __init__(self):
+    def __init__(self,telefono = None, nombre = None, direccion = None, edad = None, pedidoActivo = None, correoElectronico = None):
         super().__init__()
-
+        self._telefono = telefono
+        self._nombre = nombre
+        self._direccion = direccion
+        self._edad = edad
+        self._correoElectronico = correoElectronico
+        self._pedidoActivo = pedidoActivo
+        self._historialPedidos = []
 
         
         Cliente._clientes.append(self)
 
     # Getters y Setters
-
-    
+    def getTelefono(self):
+        return self._telefono
+    def setTelefono(self,telefono):
+        self._telefono = telefono
+    def getNombre(self):
+        return self._nombre
+    def setNombre(self, nombre):
+        self._nombre = nombre
+    def getDireccion(self):
+        return self._direccion
+    def setDireccion(self,direccion):
+        self._direccion = direccion
+    def getEdad(self):
+        return self._edad
+    def setEdad(self, edad):
+        self._edad  = edad
+    def getCorreoElectronico(self):
+        return self._correoElectronico
+    def setCorreoElectronico(self, correo):
+        self._correoElectronico = correo
+    def getHistorialPedidos(self):
+        return self._historialPedidos
+    def setHistorialPedidos(self, historial):
+        self._historialPedidos = historial
 
     @classmethod
     def getClientes(cls):
@@ -34,4 +62,9 @@ class Cliente(Usuario):
     # Implementacion de la interfaz
 
     def informacion(self):
-        pass
+        return f'El cliente {self._nombre} con email {self._correoElectronico} y telefono {self._telefono}' \
+               f'vive en la direccion {self._direccion} y tiene {self._edad} años.\n Ha hecho {len(self._historialPedidos)}' \
+               f' pedidos en la aplicacion'
+
+    def agregarPedidoHistorial(self,  pedido):
+        self._historialPedidos.append(pedido)
