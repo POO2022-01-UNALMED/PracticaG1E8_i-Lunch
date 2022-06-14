c396593eee16d1230d5e5b7e39504b2a40f317c0
Emmanuel López <lopezrodemmanuel@gmail.com>
2022-06-14 10:12:00 -0500

    Actualización Serializador/Deserializador

--- Practica2_i-Lunch/src/baseDatos/deserializador.py
+++ Practica2_i-Lunch/src/baseDatos/deserializador.py
@@ -25,46 +25,46 @@ def deserializar():
     deserializarPedidos()
 
 def deserializarEmpleados():
-    ficheroEmpleados = open(os.path.join(pathlib.Path(__file__).parent.absolute(), "temp\\empleados"),"rb") 
+    ficheroEmpleados = open(os.path.join(pathlib.Path(__file__).parent.absolute(), "temp\\empleados.txt"),"rb") 
     Empleado.setEmpleados(pickle.load(ficheroEmpleados))
     ficheroEmpleados.close()
 
 def deserializarAdministradores():
-    ficheroAdministradores = open(os.path.join(pathlib.Path(__file__).parent.absolute(), "temp\\administradores"),"rb") 
+    ficheroAdministradores = open(os.path.join(pathlib.Path(__file__).parent.absolute(), "temp\\administradores.txt"),"rb") 
     Administrador.setAdministradores(pickle.load(ficheroAdministradores))
     ficheroAdministradores.close()
 
 def deserializarRepartidores():
-    ficheroRepartidores = open(os.path.join(pathlib.Path(__file__).parent.absolute(), "temp\\repartidores"),"rb") 
+    ficheroRepartidores = open(os.path.join(pathlib.Path(__file__).parent.absolute(), "temp\\repartidores.txt"),"rb") 
     Repartidor.setRepartidores(pickle.load(ficheroRepartidores))
     ficheroRepartidores.close()
 
 def deserializarMeseros():
-    ficheroMeseros = open(os.path.join(pathlib.Path(__file__).parent.absolute(), "temp\\meseros"),"rb") 
+    ficheroMeseros = open(os.path.join(pathlib.Path(__file__).parent.absolute(), "temp\\meseros.txt"),"rb") 
     Mesero.setMeseros(pickle.load(ficheroMeseros))
     ficheroMeseros.close()
 
 def deserializarChefs():
-    ficheroChefs = open(os.path.join(pathlib.Path(__file__).parent.absolute(), "temp\\chefs"),"rb") 
+    ficheroChefs = open(os.path.join(pathlib.Path(__file__).parent.absolute(), "temp\\chefs.txt"),"rb") 
     Chef.setChefs(pickle.load(ficheroChefs))
     ficheroChefs.close()
 
 def deserializarClientes():
-    ficheroClientes = open(os.path.join(pathlib.Path(__file__).parent.absolute(), "temp\\clientes"),"rb") 
+    ficheroClientes = open(os.path.join(pathlib.Path(__file__).parent.absolute(), "temp\\clientes.txt"),"rb") 
     Cliente.setClientes(pickle.load(ficheroClientes))
     ficheroClientes.close()
 
 def deserializarRestaurantes():
-    ficheroRestaurantes = open(os.path.join(pathlib.Path(__file__).parent.absolute(), "temp\\restaurantes"),"rb") 
+    ficheroRestaurantes = open(os.path.join(pathlib.Path(__file__).parent.absolute(), "temp\\restaurantes.txt"),"rb") 
     Restaurante.setRestaurantes(pickle.load(ficheroRestaurantes))
     ficheroRestaurantes.close()
 
 def deserializarProductos():
-    ficheroProductos = open(os.path.join(pathlib.Path(__file__).parent.absolute(), "temp\\productos"),"rb") 
+    ficheroProductos = open(os.path.join(pathlib.Path(__file__).parent.absolute(), "temp\\productos.txt"),"rb") 
     Producto.setProductos(pickle.load(ficheroProductos))
     ficheroProductos.close()
 
 def deserializarPedidos():
-    ficheroPedidos = open(os.path.join(pathlib.Path(__file__).parent.absolute(), "temp\\pedidos"),"rb") 
+    ficheroPedidos = open(os.path.join(pathlib.Path(__file__).parent.absolute(), "temp\\pedidos.txt"),"rb") 
     Pedido.setPedidos(pickle.load(ficheroPedidos))
     ficheroPedidos.close()
