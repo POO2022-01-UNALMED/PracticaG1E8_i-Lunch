dd0ca1493f60a1459252c08f7be1fc0067cceaf0
Jeronimo Gomez Restrepo <jegomezre@unal.edu.co>
2022-06-14 22:47:09 -0500

    Clases Chef y Repartidor

--- Practica2_i-Lunch/src/gestorAplicacion/usuariosRestaurante/chef.py
+++ Practica2_i-Lunch/src/gestorAplicacion/usuariosRestaurante/chef.py
@@ -1,3 +1,7 @@
+from re import T
+from sympy import true
+from gestorAplicacion.gestionRestaurante.estadoPedido import EstadoPedido
+from gestorAplicacion.gestionRestaurante.pedido import Pedido
 from gestorAplicacion.usuariosRestaurante.empleado import Empleado
 
 class Chef(Empleado):
@@ -8,16 +12,27 @@ class Chef(Empleado):
 
     # Constructor
 
-    def __init__(self):
-        super().__init__()
-
-
+    def __init__(self, cedula = 0, nombre = "", disponibilidad = False, salario = 0, restaurante = None, cargoEnCocina = " ", especialidad = " "):
+        self._cargoEnCocina = cargoEnCocina
+        self._especialidad = especialidad
+        super().__init__(cedula, nombre, "Mesero", disponibilidad, salario, restaurante)
+        
 
         Chef._chefs.append(self)
 
     # Getters y Setters
+    def getCargoEnCocina(self):
+        return self._cargoEnCocina
 
+    def setCargoEnCocina(self, cargoEnCocina):
+        self._cargoEnCocina = cargoEnCocina
+    
+    def getEspecialidad(self):
+        return self._especialidad
 
+    def setEspecialidad(self, especialidad):
+        self._especialidad = especialidad
+    
 
     @classmethod
     def getChefs(cls):
@@ -29,9 +44,29 @@ class Chef(Empleado):
 
     # Metodos
 
+    def prepararProducto(self, pedido):
+        for i in range(0, len(pedido.getProductos)):
+            pedido.getProductos.get(i).setEstado(True)
+    
+    def revisionPedido(self,pedido):
+        if self._cargoEnCocina == "Chef en jefe":
+            cuenta = 0
+            for i in range(0, len(pedido.getProductos)):
+                if pedido.getProductos.get(i).getEstado() == True:
+                    cuenta += 1
+            
+            if cuenta == len(pedido.getProductos()):
+                pedido.setEstado(EstadoPedido.LISTO.value)
+
+
 
 
     # Implementacion de la interfaz
 
     def informacion(self):
-        pass
+        info = f"El Chef {self._nombre} con C.C. {self._cedula} trabaja en el restaurante {self._restaurante.getNombre()}. Tiene un salario de: ${self._salario}, tiene el cargo {self._cargo} en la cocina y esta especializado en {self.especialidad}."
+        
+        if self._disponibilidad:
+            return info + "Está disponible actualmente."
+        else:
+            return info + "No está disponible actualmente."
--- Practica2_i-Lunch/src/gestorAplicacion/usuariosRestaurante/repartidor.py
+++ Practica2_i-Lunch/src/gestorAplicacion/usuariosRestaurante/repartidor.py
@@ -1,3 +1,5 @@
+from gestorAplicacion.gestionRestaurante.estadoPedido import EstadoPedido
+from gestorAplicacion.gestionRestaurante.tipoPedido import TipoPedido
 from gestorAplicacion.usuariosRestaurante.empleado import Empleado
 
 class Repartidor(Empleado):
@@ -6,17 +8,45 @@ class Repartidor(Empleado):
 
     _repartidores = []
 
+    # Aca se guardan todos pedidos entregados por el repartidor en cuestion.
+    _pedidosEntregados = []
+
     # Constructor
 
-    def __init__(self):
-        super().__init__()
+    def __init__(self, cedula = 0, nombre = "", disponibilidad = False, salario = 0, restaurante = None, poseeVehiculo = False, placa = " ", tipoVehiculo = " "):
+        self._poseeVehiculo = poseeVehiculo
+        self._placa = placa
+        self._tipoVehiculo = tipoVehiculo
+        super().__init__(cedula, nombre, "Mesero", disponibilidad, salario, restaurante)
 
 
 
         Repartidor._repartidores.append(self)
 
     # Getters y Setters
+    def getPoseeVehiculo(self):
+        return self._poseeVehiculo
 
+    def setPoseeVehiculo(self, poseeVehiculo):
+        self._poseeVehiculo = poseeVehiculo
+
+    def getPlaca(self):
+        return self._placa
+
+    def setPlaca(self, placa):
+        self._placa = placa
+
+    def getTipoVehiculo(self):
+        return self._tipoVehiculo
+
+    def setTipoVehiculo(self, tipoVehiculo):
+        self._tipoVehiculo = tipoVehiculo
+    
+    def getPedidosEntregados(self):
+        return self._pedidosEntregados
+    
+    def setPedidosEntregados(self, pedidosEntregados):
+        self._pedidosEntregados = pedidosEntregados
     
 
     @classmethod
@@ -29,9 +59,24 @@ class Repartidor(Empleado):
 
     # Metodos
 
+    def repartirPedido(self, pedido):
+        Repartidor._pedidosEntregados.append(pedido)
+        if pedido.getEstado() == EstadoPedido.LISTO.value and pedido.getTipo() == TipoPedido.DOMICILIO.value:
+            pedido.setEstado(EstadoPedido.DESPACHADO)
 
+        
 
     # Implementacion de la interfaz
 
     def informacion(self):
-        pass
+        info = f"El Repartidor {self._nombre} con C.C. {self._cedula} trabaja en el restaurante {self._restaurante.getNombre()}. Tiene un salario de: ${self._salario}. "
+        
+        if self._poseeVehiculo:
+            return info + f"Posee un vehiculo de tipo {self._tipoVehiculo}, con placa {self._placa}."
+        else:
+            info + "No tiene vehiculo."
+
+        if self._disponibilidad:
+            return info + "Está disponible actualmente."
+        else:
+            return info + "No está disponible actualmente."
