e0d1825e0d3f5b95e4dba70ae02006d25575286c
Davidcito12 <92931033+Davidcito12@users.noreply.github.com>
2022-06-15 02:35:50 -0500

    Update

--- Practica2_i-Lunch/src/gestorAplicacion/gestionRestaurante/pedido.py
+++ Practica2_i-Lunch/src/gestorAplicacion/gestionRestaurante/pedido.py
@@ -3,17 +3,20 @@ class Pedido:
     # Atributos estaticos
 
     _pedidos = []
-
+    _totalPedidos = 0
     # Constructor
 
-    def __init__(self):
-
-
+    def __init__(self, cliente = None, codigo = None, tipo = None, estado = None, mensaje = None,fechaHora = None, precioTotal = None, restaurante = None):
+        self._cliente = cliente
+        self._codigo = codigo
+        self._tipo = tipo
+        self._estado = estado
+        self._productos = []
+        self._mensaje = mensaje
+        self._fechaHora = fechaHora
+        self._precioTotal = precioTotal
+        self._restaurante = restaurante
         Pedido._pedidos.append(self)
-
-    # Getters y Setters
-
-    
 
     @classmethod
     def getPedidos(cls):
@@ -23,5 +26,84 @@ class Pedido:
     def setPedidos(cls, pedidos):
         cls._pedidos = pedidos
 
+    @classmethod
+    def getTotalPedidos(cls):
+        return cls._totalPedidos
+
+    @classmethod
+    def setTotalPedidos(cls, totalPedidos):
+        cls._totalPedidos = totalPedidos
+
     # Metodos
     
+    def calcularPrecioTotal(self):
+        sum = 0
+        for i in self._productos:
+            sum += i.getPrecio()
+        return sum
+
+
+    # Getters y Setters
+
+    def getCliente(self):
+        return self._cliente
+
+    def setCliente(self, nuevoCliente):
+        self._cliente = nuevoCliente
+
+    def getCodigo(self):
+        return self._codigo
+
+    def setCodigo(self, codigo):
+        self._codigo = codigo
+
+    def getEstado(self):
+        return self._estado
+
+    def setEstado(self, estado):
+        self._estado = estado
+
+    def getProductos(self):
+        return self._productos
+
+    def setProductos(self, productos):
+        self._productos = productos
+
+    def getTipo(self):
+        return  self._tipo
+
+    def setTipo(self, tipo):
+        self._tipo = tipo
+
+    def getMensaje(self):
+        return self._mensaje
+
+    def setMensaje(self, mensaje):
+        self._mensaje = mensaje
+
+    def getFechaHora(self):
+        return self._fechaHora
+
+    def setFechaHora(self, fechaHora):
+        self._fechaHora = fechaHora
+
+    def getPrecioTotal(self):
+        return self._precioTotal
+
+    def setPrecioTotal(self, precioTotal):
+        self._precioTotal = precioTotal
+
+    def getRestaurante(self):
+        return self._restaurante
+
+    def setRestaurante(self, restaurante):
+        self._restaurante = restaurante
+
+
+    def __str__(self):
+        return f'Pedido {self._codigo} hecho por el cliente {self._cliente} \n' \
+               f'Fecha y hora: {self._fechaHora} \n' \
+               f'Estado: {self._estado} \n' \
+               f'Mensaje: {self._mensaje} \n' \
+               f'Tipo: {self._tipo}\n' \
+               f'Precio total: {self._precioTotal}'
--- Practica2_i-Lunch/src/gestorAplicacion/gestionRestaurante/producto.py
+++ Practica2_i-Lunch/src/gestorAplicacion/gestionRestaurante/producto.py
@@ -6,14 +6,56 @@ class Producto:
 
     # Constructor
 
-    def __init__(self):
-
+    def __init__(self, nombre = None, descripcino = None, precio = None, disponibilidad = None, restriccion = None, cantidad = None):
+        self._nombre = nombre
+        self._descripcion = descripcino
+        self._precio = precio
+        self._restriccion = restriccion
+        self._disponibilidad= disponibilidad
+        self._cantidad = cantidad
+        self._estado = False
 
         Producto._productos.append(self)
 
     # Getters y Setters
+    def getNombre(self):
+        return self._nombre
 
-    
+    def setNombre(self, nombre):
+        self._nombre = nombre
+
+    def getDescripcion(self):
+        return self._descripcion
+
+    def setDescripcion(self, descripcion):
+        self._descripcion = descripcion
+
+    def getPrecio(self):
+        return self._precio
+
+    def setPrecio(self, precio):
+        self._precio = precio
+
+    def getDisponibilidad(self):
+        return self._disponibilidad
+
+    def setDisponibilidad(self, disponibilidad):
+        self._disponibilidad = disponibilidad
+
+    def getRestriccion(self):
+        return self._restriccion
+
+    def setRestriccion(self, restriccion):
+        self._restriccion =  restriccion
+
+    def getCantidad(self):
+        return self._cantidad
+
+    def setCantidad(self, cantidad):
+        self._cantidad = cantidad
+
+    def getEstado(self):
+        return self._estado
 
     @classmethod
     def getProductos(cls):
@@ -25,4 +67,11 @@ class Producto:
 
     # Metodos
 
+    def __str__(self):
+        return f'Nombre: {self._nombre} \n' \
+               f'Descripcion: {self._descripcion} \n' \
+               f'Precio: {self._precio}\n' \
+               f'Disponibilidad: {self._disponibilidad} \n' \
+               f'Restriccion de edad: {self._restriccion}\n' \
+               f'Cantidad disponible: {self._cantidad}'
     
